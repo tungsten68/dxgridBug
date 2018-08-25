59672d0ad104bcda40400044190669b350365b97
Wolfram Steiner <wolfram@steiner.net>
2018-08-25 07:47:27 +0200

    Everything

---
+++ dxgridBug/Models/Contact.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace dxgridBug.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+    }
+
+    public class ContactWithAtt
+    {
+        [Display(GroupName = "ID")]
+        public int Id { get; set; }
+        [Display(GroupName = "ID")]
+        public string Code { get; set; }
+        [Display(GroupName = "Name")]
+        public string Name { get; set; }
+        [Display(GroupName = "Adress")]
+        public string City { get; set; }
+    }
+
+}
---
+++ dxgridBug/ViewModels/ContactViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using dxgridBug.Models;
+using DevExpress.Mvvm.DataAnnotations;
+using DevExpress.Mvvm;
+
+namespace dxgridBug.ViewModels
+{
+    [POCOViewModel]
+    public class ContactViewModel
+    {
+        public ContactViewModel()
+        {
+            Items = Seed("JG", "Jennifer Grogl", "Schladming");
+            ItemsWithAtt = SeedWithAtt("JGWA", "Jennifer Grogl With Att", "Schladming");
+        }
+
+        public virtual ObservableCollection<Contact> Items { get; set; }
+        public virtual Contact Current { get; set; }
+        public virtual ObservableCollection<ContactWithAtt> ItemsWithAtt { get; set; }
+        public virtual Contact CurrentWittAtt { get; set; }
+
+        public void Search()
+        {
+            Items = Seed("WS", "Wolfram Steiner", "Groebming");
+            ItemsWithAtt = SeedWithAtt("WSWA", "Wolfram Steiner With Att", "Groebming");
+        }
+
+        private ObservableCollection<Contact> Seed(string code, string name, string city)
+        {
+            var coll = new ObservableCollection<Contact>();
+            for (int i = 1; i < 50; i++)
+            {
+                coll.Add(new Contact { Code = code + i, Name = name + i, City = city });
+            }
+
+            return coll;
+        }
+
+        private ObservableCollection<ContactWithAtt> SeedWithAtt(string code, string name, string city)
+        {
+            var coll = new ObservableCollection<ContactWithAtt>();
+            for (int i = 1; i < 50; i++)
+            {
+                coll.Add(new ContactWithAtt { Code = code + i, Name = name + i, City = city });
+            }
+            return coll;
+        }
+    }
+}
